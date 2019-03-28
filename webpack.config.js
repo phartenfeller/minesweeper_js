fc1ada1fecf611c2e5ee4a383a40a859e15c780b
phartenfeller <phartenfeller@mtags-MacBook-Pro.local>
2019-03-28 12:33:35 +0100

    GH Pages included to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
+  plugins: [
+    new CopyPlugin([
+      { from: './index.html', to: './' },
+      { from: './src/css', to: './src/css' },
+      { from: './sprites', to: './sprites/' },
+    ]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
