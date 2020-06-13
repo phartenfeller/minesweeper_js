0768aea0e6fb47e18921f6668d8a666688c8a58a
phartenfeller <pharti@hotmail.de>
2020-06-13 11:45:55 +0200

    fix #15

--- src/js/db/changeBestGames.js
+++ src/js/db/changeBestGames.js
@@ -3,7 +3,11 @@ import { showTime } from '../util';
 
 const changeBestGame = async mode => {
   const db = getDb();
-  const bestGames = await db.games.where('mode').equals(mode).sortBy('time');
+  const bestGames = await db.games
+    .where('mode')
+    .equals(mode)
+    .and(game => game.result === 'w') // can't chain multiple wheres unfortunately: https://github.com/dfahlander/Dexie.js/issues/427
+    .sortBy('time');
   const best5 = bestGames.slice(0, 5);
   showTime(best5, '#best-scores tbody');
 };
