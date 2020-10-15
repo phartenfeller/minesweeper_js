fd685a69008c2eda9d8d4c9f61ca7d3db271ae6b
Philipp Hartenfeller <34486873+phartenfeller@users.noreply.github.com>
2020-10-15 07:32:41 +0000

    extract console statements and upgraded webpack copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -7,52 +7,63 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyPlugin([
-      { from: './assets', to: './assets/' },
-      { from: './favicon.ico', to: './' },
-      { from: './manifest.json', to: './' },
-      { from: './pwabuilder-sw.js', to: './' },
-      { from: './install.js', to: './' },
-      { from: './sitemap.xml', to: './' }
-    ]),
-    // new BundleAnalyzerPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
-      chunkFilename: 'styles.[contenthash].css'
-    }),
-    new HtmlWebpackPlugin({ filename: 'index.html', template: 'index.html' })
-  ],
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader'
+module.exports = (env, argv) => {
+  console.log(`Webpack mode => ${argv.mode}`);
+  return {
+    entry: './src/index.js',
+    plugins: [
+      new CleanWebpackPlugin(),
+      new CopyPlugin({
+        patterns: [
+          { from: './assets', to: './assets/' },
+          { from: './favicon.ico', to: './' },
+          { from: './manifest.json', to: './' },
+          { from: './pwabuilder-sw.js', to: './' },
+          { from: './install.js', to: './' },
+          { from: './sitemap.xml', to: './' }
         ]
-      }
-    ]
-  },
-  // Optional for webpack-dev-server
-  devServer: {
-    watchContentBase: true,
-    contentBase: path.resolve(__dirname, 'dist'),
-    open: true
-  }
+      }),
+      // new BundleAnalyzerPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'styles.[contenthash].css',
+        chunkFilename: 'styles.[contenthash].css'
+      }),
+      new HtmlWebpackPlugin({ filename: 'index.html', template: 'index.html' })
+    ],
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserJSPlugin({
+          extractComments: env === 'production'
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.[contenthash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader'
+          ]
+        }
+      ]
+    },
+    // Optional for webpack-dev-server
+    devServer: {
+      watchContentBase: true,
+      contentBase: path.resolve(__dirname, 'dist'),
+      open: true
+    }
+  };
 };
