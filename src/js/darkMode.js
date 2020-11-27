e25da32898ba7ea6ff2a66a20e69e69d7db627c5
phartenfeller <pharti@hotmail.de>
2020-11-27 16:30:36 +0100

    added theme toggle

--- src/js/darkMode.js
+++ src/js/darkMode.js
@@ -1,3 +1,11 @@
+import { changeClass } from './util';
+
+let darkState;
+let themeToggle;
+let themeToggleThumb;
+const lightTranslate = 'translate-x-0';
+const darkTranslate = 'translate-x-7';
+
 const setLightMode = () => {
   document.querySelector('html').classList.remove('dark');
   localStorage.theme = 'light';
@@ -8,16 +16,43 @@ const setDarkMode = () => {
   localStorage.theme = 'dark';
 };
 
+const applySetting = () => {
+  if (darkState) {
+    changeClass(themeToggleThumb, lightTranslate, darkTranslate);
+    themeToggle.setAttribute('aria-checked', true);
+    setDarkMode();
+  } else {
+    changeClass(themeToggleThumb, darkTranslate, lightTranslate);
+    themeToggle.setAttribute('aria-checked', false);
+    setLightMode();
+  }
+};
+
+const initListeners = () => {
+  /* Touch device theme toggle listeners */
+  themeToggle = document.getElementById('theme-toggle');
+  themeToggleThumb = document.getElementById('theme-toggle-thumb');
+
+  themeToggle.addEventListener('click', () => {
+    darkState = !darkState;
+    applySetting();
+  });
+};
+
 const initDarkMode = () => {
   if (
     localStorage.theme === 'dark' ||
     (!('theme' in localStorage) &&
       window.matchMedia('(prefers-color-scheme: dark)').matches)
   ) {
+    darkState = true;
     setDarkMode();
   } else {
+    darkState = false;
     setLightMode();
   }
+
+  initListeners();
 };
 
 export { setDarkMode, setLightMode, initDarkMode };
